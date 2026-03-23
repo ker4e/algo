837b8cf13b2e28d63e22cddcdaddf60dc4a79bdc
ker4e <rus.shirokov.00@bk.ru>
2026-03-23 12:55:36 -0700

    Create 2.2.1.cpp

---
+++ 2.2.1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int Fibonacci(int* arr, int n);
+
+int main() 
+{
+	int size{ 25 };
+	int* arrayFibo = new int[size];
+	std::cout << "Fibonacci: ";
+
+	for (int i{ 0 }; i < size; ++i)
+	{
+		arrayFibo[i] = -1;
+	}
+
+	for (int i{ 0 }; i < size; ++i)
+	{
+		std::cout << Fibonacci(arrayFibo, i) << ' ';
+	}
+
+	std::cout << "\n\nFibonacci after add to array: ";
+	for (int i{ 0 }; i < size; ++i)
+	{
+		std::cout << Fibonacci(arrayFibo, i) << ' ';
+	}
+
+
+	delete[] arrayFibo;
+
+	return EXIT_SUCCESS;
+}
+
+int Fibonacci(int* arr, int n)
+{
+	if (n <= 0)return 0;
+	else if (n == 1) return 1;
+
+	if (arr[n] != -1)
+	{
+		return arr[n];
+	}
+
+	arr[n] = Fibonacci(arr, n - 1) + Fibonacci(arr, n - 2);
+	return arr[n];
+}
