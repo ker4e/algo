e99a62a55d37001834905449f5cd0648d51e936b
ker4e <rus.shirokov.00@bk.ru>
2026-03-23 12:57:28 -0700

    Create 2.2.2.cpp

---
+++ 2.2.2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+int Fibonacci(std::vector<int>& arr, int n);
+
+int main()
+{
+    int size = 25;
+    std::vector<int> arr(size, -1);
+
+    std::cout << "Fibonacci: ";
+    for (int i = 0; i < size; ++i)
+        std::cout << Fibonacci(arr, i) << " ";
+}
+
+int Fibonacci(std::vector<int>& arr, int n)
+{
+    if (n <= 1) return n;
+
+    if (arr[n] != -1)
+        return arr[n];
+
+    arr[n] = Fibonacci(arr, n - 1) + Fibonacci(arr, n - 2);
+    return arr[n];
+}
