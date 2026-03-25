e1d7b267e13eea598f7dfbf438d122a885e1c594
ker4e <rus.shirokov.00@bk.ru>
2026-03-25 12:35:20 -0700

    Update 3.2.cpp

--- 3.2.cpp
+++ 3.2.cpp
@@ -8,31 +8,31 @@ int main()
 	int arrayOne[]{ 3, 43, 38, 29, 18, 72, 57, 61, 2, 33 };
 	int sizeOne{ sizeof(arrayOne) / sizeof(arrayOne[0]) };
 
-	std::cout << "Array one before merge sort: ";
+	std::cout << "Array one before quick sort: ";
 	printArr(arrayOne, sizeOne);
 	quick_sort(arrayOne, sizeOne);
 
-	std::cout << "Array one after merge sort: ";
+	std::cout << "Array one after quick sort: ";
 	printArr(arrayOne, sizeOne);
 
 	int arrayTwo[]{ 88, 91, 87, 59, 53, 49, 29, 16, 4, 27, 28, 89, 2, 25, 74 };
 	int sizeTwo{ sizeof(arrayTwo) / sizeof(arrayTwo[0]) };
 
-	std::cout << "\nArray two before merge sort: ";
+	std::cout << "\nArray two before quick sort: ";
 	printArr(arrayTwo, sizeTwo);
 	quick_sort(arrayTwo, sizeTwo);
 
-	std::cout << "Array two after merge sort: ";
+	std::cout << "Array two after quick sort: ";
 	printArr(arrayTwo, sizeTwo);
 
 	int arrayThree[]{ 24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
 	int sizeThree{ sizeof(arrayThree) / sizeof(arrayThree[0]) };
 
-	std::cout << "\nArray three before merge sort: ";
+	std::cout << "\nArray three before quick sort: ";
 	printArr(arrayThree, sizeThree);
 	quick_sort(arrayThree, sizeThree);
 
-	std::cout << "Array three after merge sort: ";
+	std::cout << "Array three after quick sort: ";
 	printArr(arrayThree, sizeThree);
 
 	return EXIT_SUCCESS;
