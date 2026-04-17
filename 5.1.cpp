0b3c1faaae8e4298091d021d163fa71076137801
ker4e <rus.shirokov.00@bk.ru>
2026-04-17 21:02:52 +0300

    Add simple string hash function and main loop
    Implement a simple string hashing function and user input loop.

---
+++ 5.1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int simple_string_hash(const std::string& s) {
+    int hash_value = 0;
+
+    for (size_t i = 0; i < s.size(); ++i) {
+        hash_value += s[i];
+    }
+    return hash_value;
+}
+
+int main() {
+    std::string input;
+
+    while (true) {
+        std::cout << "Enter a string: ";
+        std::getline(std::cin, input);
+
+        int hash_result = simple_string_hash(input);
+        std::cout << "Simple hash of string ' " << input << " ' = " << hash_result << std::endl;
+      
+        if (input == "exit") {
+            break;
+        }
+    }
+    return 0;
+}
