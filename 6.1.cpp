644e6f394fe4c1ee7bf37edbeeae8d1b76f1991d
ker4e <rus.shirokov.00@bk.ru>
2026-04-29 21:07:16 +0300

    Update 6.1.cpp

--- 6.1.cpp
+++ 6.1.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 
+void print_array(const int* arr, int size);
+int get_level(int index);
+void print_pyramid(const int* arr, int size);
+
+int main() {
+    int arr1[] = {1, 3, 6, 5, 9, 8};
+    int arr2[] = {94, 67, 18, 44, 55, 12, 6, 42};
+    int arr3[] = {16, 11, 9, 10, 5, 6, 8, 1, 2, 4};
+
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+    int size3 = sizeof(arr3) / sizeof(arr3[0]);
+
+    print_array(arr1, size1);
+    print_pyramid(arr1, size1);
+
+    std::cout << std::endl;
+
+    print_array(arr2, size2);
+    print_pyramid(arr2, size2);
+
+    std::cout << std::endl;
+
+    print_array(arr3, size3);
+    print_pyramid(arr3, size3);
+
+    return 0;
+}
+
 void print_array(const int* arr, int size) 
 {
     std::cout << "Original array: ";
@@ -50,29 +79,4 @@ void print_pyramid(const int* arr, int size)
             std::cout << level << " right(" << arr[parent_index] << ") " << arr[i] << std::endl;
         }
     }
-}
-
-int main() {
-    int arr1[] = {1, 3, 6, 5, 9, 8};
-    int arr2[] = {94, 67, 18, 44, 55, 12, 6, 42};
-    int arr3[] = {16, 11, 9, 10, 5, 6, 8, 1, 2, 4};
-
-    int size1 = sizeof(arr1) / sizeof(arr1[0]);
-    int size2 = sizeof(arr2) / sizeof(arr2[0]);
-    int size3 = sizeof(arr3) / sizeof(arr3[0]);
-
-    print_array(arr1, size1);
-    print_pyramid(arr1, size1);
-
-    std::cout << std::endl;
-
-    print_array(arr2, size2);
-    print_pyramid(arr2, size2);
-
-    std::cout << std::endl;
-
-    print_array(arr3, size3);
-    print_pyramid(arr3, size3);
-
-    return 0;
 }
